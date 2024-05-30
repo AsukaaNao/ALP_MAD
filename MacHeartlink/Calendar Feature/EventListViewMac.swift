df65eeb9fdb715da927f1d529288c938a466b0a1
Shelfinna <77188043+shelfinnaa@users.noreply.github.com>
2024-05-30 18:52:22 +0700

    Calendar View MacOS Gajelas

---
+++ MacHeartlink/Calendar Feature/EventListViewMac.swift
@@ -0,0 +1,18 @@
+//
+//  EventListViewMac.swift
+//  MacHeartlink
+//
+//  Created by Shelfinna on 30/05/24.
+//
+
+import SwiftUI
+
+struct EventListViewMac: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventListViewMac()
+}
---
+++ MacHeartlink/Calendar Feature/EventsCalendarViewMac.swift
@@ -0,0 +1,18 @@
+//
+//  EventsCalendarViewMac.swift
+//  MacHeartlink
+//
+//  Created by Shelfinna on 30/05/24.
+//
+
+import SwiftUI
+
+struct EventsCalendarViewMac: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsCalendarViewMac()
+}
